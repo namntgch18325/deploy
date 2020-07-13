a15fbde3811edb630ef72be473d29346f394e082
namntgch18325 <namntgch18325@fpt.edu.vn>
2020-07-13 13:00:58 +0700

    update

--- Routes/brand.routes.js
+++ Routes/brand.routes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const Controller = require("../Controller/brand.controller.js");
 
 router.post("/addNewBrand",Controller.addNewBrand);
-
+router.get("getBrand",Controller.getBrand);
 module.exports = router;
