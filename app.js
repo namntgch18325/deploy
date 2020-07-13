9ef01161b53655b6f207b66fbf54e10bf5511365
namntgch18325 <namntgch18325@fpt.edu.vn>
2020-07-13 12:22:10 +0700

    update file app

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
-
+const dotenv = require('dotenv').config();
 const bodyParser = require("body-parser");
 
 const routes = require("./Routes/routes.js");
+const routerCart = require("./Routes/cartRoutes.js")
+const routerAuthSWT = require("./Routes/auth.jwt.routes")
+const routerBrand = require("./Routes/brand.routes.js")
 
 const corsList = {
     origin: "http://localhost:8080"
@@ -13,9 +16,12 @@ const corsList = {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(cors(corsList));
+//app.use(dotenv);
+app.use("/",routerAuthSWT);
 app.use("/",routes);
+app.use("/",routerCart);
+app.use("/",routerBrand);
 
 app.listen(3000, function log() {
     console.log("Server Is Runing, Port 3000");
